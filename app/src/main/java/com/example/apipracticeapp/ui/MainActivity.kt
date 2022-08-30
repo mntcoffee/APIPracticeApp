33a1247646625f3ca42e084f8d165dacd4670c90
stakasaki <takasaki.shogo707@mail.kyutech.jp>
2022-08-30 20:51:56 +0900

    activityにnavControllerを追加

--- app/src/main/java/com/example/apipracticeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/apipracticeapp/ui/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.apipracticeapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.apipracticeapp.R
+import com.example.apipracticeapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        val navView: BottomNavigationView = binding.bottomNavBar
+        val navController = findNavController(R.id.fragmentContainerView2)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.rankingFragment, R.id.accountFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
